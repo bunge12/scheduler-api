b470aa6c1eeb465d64c78f601f3b816f52c8a39d
Artur <54365622+bunge12@users.noreply.github.com>
2020-05-11 11:46:23 -0700

    Updated port

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
-const PORT = process.env.PORT || 8001;
+let PORT = process.env.PORT;
+if (PORT == null || PORT == "") {
+  PORT = 8001;
+}
+
 const ENV = require("./environment");
 
 const app = require("./application")(ENV, { updateAppointment });
@@ -7,8 +11,8 @@ const server = require("http").Server(app);
 const WebSocket = require("ws");
 const wss = new WebSocket.Server({ server });
 
-wss.on("connection", socket => {
-  socket.onmessage = event => {
+wss.on("connection", (socket) => {
+  socket.onmessage = (event) => {
     console.log(`Message Received: ${event.data}`);
 
     if (event.data === "ping") {
@@ -24,7 +28,7 @@ function updateAppointment(id, interview) {
         JSON.stringify({
           type: "SET_INTERVIEW",
           id,
-          interview
+          interview,
         })
       );
     }
